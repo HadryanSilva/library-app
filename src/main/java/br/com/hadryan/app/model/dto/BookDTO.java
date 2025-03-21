b4cb804812ecc10805e3d1ec0530f305b935d619
Hadryan Silva <hadryan2001@gmail.com>
2025-03-21 14:19:28 -0300

    chore: add DTOs for entities

--- src/main/java/br/com/hadryan/app/model/dto/BookDTO.java
+++ src/main/java/br/com/hadryan/app/model/dto/BookDTO.java
@@ -1,4 +1,57 @@
 package br.com.hadryan.app.model.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class BookDTO {
+
+    private String title;
+
+    private String author;
+
+    private LocalDate publishDate;
+
+    private List<PublisherDTO> publishers;
+
+    private List<AuthorDTO> authors;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public List<AuthorDTO> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<AuthorDTO> authors) {
+        this.authors = authors;
+    }
+
+    public List<PublisherDTO> getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(List<PublisherDTO> publishers) {
+        this.publishers = publishers;
+    }
 }
--- src/main/java/br/com/hadryan/app/model/dto/PublisherDTO.java
+++ src/main/java/br/com/hadryan/app/model/dto/PublisherDTO.java
@@ -1,4 +1,14 @@
 package br.com.hadryan.app.model.dto;
 
 public class PublisherDTO {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
